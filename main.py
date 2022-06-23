91f81f90bd1983388c3e34246dc514b032a37afc
alexFocus92 <alex92vk@gmail.com>
2022-06-23 23:36:35 +0200

    Subida inicial de codigo

---
+++ main.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D, Dropout
+from keras.optimizers import Adam, SGD
+from keras.metrics import categorical_crossentropy
+from keras.preprocessing.image import ImageDataGenerator
+import warnings
+import numpy as np
+import cv2
+from keras.callbacks import ReduceLROnPlateau
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from matplotlib import pyplot as plt
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+train_path = './train'
+test_path = './test'
+train_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(directory=train_path, target_size=(64,64), class_mode='categorical', batch_size=10,shuffle=True)
+test_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(directory=test_path, target_size=(64,64), class_mode='categorical', batch_size=10, shuffle=True)
+
+imgs, labels = next(train_batches)
+#Plotting the images...
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 10, figsize=(30,20))
+    axes = axes.flatten()
+    for img, ax in zip( images_arr, axes):
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        ax.imshow(img)
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+plotImages(imgs)
+print(imgs.shape)
+print(labels)
+
+model = Sequential()
+model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(64,64,3)))
+model.add(MaxPool2D(pool_size=(2, 2), strides=2))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding = 'same'))
+model.add(MaxPool2D(pool_size=(2, 2), strides=2))
+model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding = 'valid'))
+model.add(MaxPool2D(pool_size=(2, 2), strides=2))
+model.add(Flatten())
+model.add(Dense(64,activation ="relu"))
+model.add(Dense(128,activation ="relu"))
+#model.add(Dropout(0.2))
+model.add(Dense(128,activation ="relu"))
+#model.add(Dropout(0.3))
+model.add(Dense(10,activation ="softmax"))
+
+model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=1, min_lr=0.0001)
+early_stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=2, verbose=0, mode='auto')
+model.compile(optimizer=SGD(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=1, min_lr=0.0005)
+early_stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=2, verbose=0, mode='auto')
+
+history2 = model.fit(train_batches, epochs=10, callbacks=[reduce_lr, early_stop],  validation_data = test_batches)
